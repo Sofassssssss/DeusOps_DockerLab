920263ab49911c982a1b5c1d4984ce8fcd029424
Sofia Vovchenko <112688206+Sofassssssss@users.noreply.github.com>
2024-12-09 21:19:06 +0300

    Delete Webby directory

--- Webby/data/__all_models.py
+++
@@ -1,7 +0,0 @@
-from .models.user import User
-from .models.content import Content
-from .models.message import Message
-from .models.avatars import Avatar
-from .models.advertisements import Advertisement
-from .models.interests import Interest
-from .models.ranks import Rank
